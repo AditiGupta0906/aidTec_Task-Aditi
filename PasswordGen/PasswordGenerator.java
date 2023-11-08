8b958221dbe71b25d6c84c5817105cec20deefcf
AditiGupta0906 <142398027+AditiGupta0906@users.noreply.github.com>
2023-11-08 23:14:16 +0530

    Add files via upload
    This code provides a simple GUI for temperature conversion with a clear interface and user-friendly interaction. The application can handle user input errors and supports converting between Celsius, Fahrenheit, and Kelvin scales.

--- PasswordGen/PasswordGenerator.java
+++ PasswordGen/PasswordGenerator.java
@@ -1,25 +1,31 @@
 import java.security.SecureRandom;
 import java.util.Scanner;
+// import necessary Java classes. 
+// SecureRandom is used for secure random number generation, and Scanner is used for user input.
 
 public class PasswordGenerator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //The main method is the entry point of the program. It creates a Scanner object to read user input.
 
         // Step 1: Define the length of the password
         System.out.print("Enter the length of the password: ");
-        int passwordLength = scanner.nextInt();
+        int passwordLength = scanner.nextInt();     
+        // This part prompts the user to enter the desired length of the password.
 
         // Step 2: Create a character set
         String characterSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*/=+";
+        // Here, a character set is defined, which includes uppercase letters, lowercase letters, numbers, and special characters.
 
         // Step 3: Create a function to generate the password
-        String generatedPassword = generatePassword(passwordLength, characterSet);
+        String generatedPassword = generatePassword(passwordLength, characterSet);     
+        // The generatePassword method is called to generate a password based on the specified length and character set.
 
         // Step 7: Display the generated password
         System.out.println("Generated Password: " + generatedPassword);
+        // The generated password is then printed to the console.
     }
-
     // Step 4: Use random number generation
     private static String generatePassword(int length, String characterSet) {
         SecureRandom random = new SecureRandom();
@@ -31,6 +37,9 @@ public class PasswordGenerator {
             char randomChar = characterSet.charAt(randomIndex);
             password.append(randomChar);
         }
+        // This is the generatePassword method. It uses a loop to randomly select characters from 
+        // the character set and appends them to a StringBuilder to form the password. 
+        // The resulting password is then converted to a string and returned.
 
         return password.toString();
     }
